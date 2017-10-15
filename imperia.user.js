45375d869437f7e39f463993dab6d7f9150c4d02
szpaaq <szpaaaaq@gmail.com>
2017-10-15 13:07:58 +0200

    first commit

--- imperia.user.js
+++ imperia.user.js
@@ -1,4 +1,4 @@
 window.onload=function(){
   var url = 'https://rawgit.com/SzpaaQ/imperia/master/SQ.js';
-  $('body').append('<div id="SQ_script"><script type="text/javascript" src="'+url+'"></script></div>');
+  $('body').append('<div id="SQ_script"><link type="text/css" rel="stylesheet" href="https://rawgit.com/SzpaaQ/imperia/master/sq.css"><script type="text/javascript" src="'+url+'"></script></div>');
 }
