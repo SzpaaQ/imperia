02f69affb54ab18a99204d50e27dac2d3164317a
szpaaq <szpaaaaq@gmail.com>
2017-10-21 19:57:30 +0200

    first commit

--- SQ.js
+++ SQ.js
@@ -4,10 +4,14 @@ var getArmy				= function(step)
 {
 	
 }
+
 var SQ_addBtn 			= function(text, fn,c){SQ_btns.push('<div class="'+c+' pull-right" onclick="'+fn+'()">'+text+'</div>');}
 var SQ_commitBtns 		= function(){SQ_addButtons();$('.ui-bottom-right').append(SQ_btns.join(''));}
 var SQ_autoattack 		= function(){if(SQ_attackWorking == false){SQ_otherInterval = setInterval(function(){spy()},1000);$('.SQ_startattack').find('.ui-buttons').css('background-color','rgba(255,255,255,.7)');SQ_attackWorking = true;}else{clearInterval(SQ_otherInterval);$('.SQ_startattack').find('.ui-buttons').css('background-color','rgba(0,0,0,0)');SQ_attackWorking = false;}}
-var spy 				= function(lvl){if($('#widget-missions').find('.mission-my').length) return;if(!lvl) lvl = 7;xajax_doMassSpy(container.open({saveName:'mass-spy-report-100'+lvl, title:'Masowe szpiegostwo: Niezależne miasto'}),{'uId':1000+lvl,'isUserHolding':'1', 'page': 1});clearInterval(SQ_otherInterval);SQ_otherInterval=setInterval(function(){SQ_setVictim()},1000)}
+var spy = function(lvl){if($('#widget-missions').find('.mission-my').length) return;if(!lvl) lvl = 7;xajax_doMassSpy(container.open({saveName:'mass-spy-report-100'+lvl, title:'Masowe szpiegostwo: Niezależne miasto'}),{'uId':1000+lvl,'isUserHolding':'1', 'page': 1});clearInterval(SQ_otherInterval);SQ_otherInterval=setInterval(function(){SQ_chooseVictim()},1000)}
+var openTwierdza 		= function(){if($('#widget-missions').find('.mission-my').length) return;$('body').trigger(jQuery.Event( 'keyup', { keyCode: 67, which: 67 } ));clearInterval(SQ_otherInterval);SQ_otherInterval=setInterval(function(){SQ_chooseVictim()},1000)}
+var openSpies 			= function(){if(!$('#operation-center').find('.tab-espionage').find('a').length)return;clearInterval(SQ_otherInterval);$('#operation-center').find('.tab-espionage').find('a')[0].click();SQ_otherInterval = setInterval(function(){SQ_infiltrate()},1000)}
+var SQ_infiltrate 		= function(){if(!$('.infiltrate-all').length)return;clearInterval(SQ_otherInterval);$('.infiltrate-all')[0].click();SQ_otherInterval = setInterval(function(){SQ_chooseVictim()},1000)}
 var SQ_setVictim 		= function(v){SQ_chosen = v.find('.attack');}
 var SQ_chooseVictim 	= function(){if(!$('.spy-mass-report-content').length)return;clearInterval(SQ_otherInterval);var rows = $('.spy-mass-report-content').find('table').first().find('tr').find('.prov-pict');var lenn = rows.length;$.each(rows, function(index){if(!$(this).closest('tr').find('td.wood').length) return;var text = parseInt($(this).closest('tr').find('td.wood').attr('title').trim().replace('&nbsp;', '').replace(',0K', '000').replace(',0M', '000'));if(text > SQ_most){SQ_most = text;SQ_setVictim($(this).closest('tr'));}if(index == lenn-1){SQ_chosen.click();SQ_otherInterval = setInterval(function(){SQ_FillUpArmy()},1000);SQ_most = 0;}});}
 var SQ_FillUpArmy 		= function(){if(!$('#select-all-army').length)return;clearInterval(SQ_otherInterval);$("#select-all-army").click();$('.ui-pass[name=formation]').val(formation).trigger('change');	SQ_otherInterval = setInterval(function(){SQ_sendAttack()},1000)}
--- twierdza.js
+++ twierdza.js
@@ -1,13 +1,12 @@
 var x = [];
-xjxfun:doMassSpy
-x.push('Soperation-center-map');
+x.push('SOperationCenter');
 var d = new Date();
 var n = d.getTime();
-x.push(n);
-x.push('<xjxobj><e><k>cX</k><v>N2001</v></e><e><k>cY</k><v>N-900</v></e><e><k>id</k><v>Swb9700</v></e><e><k>type</k><v>N9701</v></e><e><k>w</k><v>N7</v></e><e><k>x</k><v>N2001</v></e><e><k>y</k><v>N-900</v></e><e><k>vexok</k><v>Btrue</v></e></xjxobj>');
+x.push('<xjxobj><e><k>generalId</k><v>S27635</v></e><e><k>army</k><v><xjxobj><e><k>P1</k><v>S</v></e><e><k>P2</k><v>S</v></e><e><k>CT</k><v>S</v></e><e><k>S1</k><v>S1001</v></e><e><k>S2</k><v>S</v></e><e><k>C1</k><v>S</v></e><e><k>M1</k><v>S</v></e><e><k>M2</k><v>S</v></e><e><k>C2</k><v>S</v></e><e><k>K2</k><v>S</v></e><e><k>C4</k><v>S</v></e></xjxobj></v></e><e><k>dUserId</k><v>S1007</v></e><e><k>dProvinceId</k><v>S1662</v></e><e><k>attackType</k><v>S1</v></e><e><k>dName</k><v>SNPCHeavyGod_7</v></e><e><k>formation</k><v>S1</v></e><e><k>sendAttack</k><v>S<![CDATA[Oblężenie Twierdzy]]></v></e><e><k>vexok</k><v>Btrue</v></e></xjxobj>');
 $.post('xajax_loader.php',
 {
-	xjxfun:'sendAttackWorldboss',
+	xjxfun:'doAttack',
+	xjxr: n,
 	xjxargs:x
 },
 function(data){
@@ -20,7 +19,37 @@ function(data){
 var x = [];
 x.push('mass-spy-report-1007');
 
-var spy = function(lvl){if(!lvl) lvl = 7;xajax_doMassSpy(container.open({saveName:'mass-spy-report-100'+lvl, title:'Masowe szpiegostwo: Niezależne miasto'}),{'uId':100+lvl,'isUserHolding':'1', 'page': 1});}
+var spy = function(lvl){if(!lvl) lvl = 7;
+	javascript:void(xajax_doCancelTransportMission('missions', {missionId: 444668}));
+	xajax_doMassSpy(container.open({saveName:'mass-spy-report-100'+lvl, title:'Masowe szpiegostwo: Niezależne miasto'}),{'uId':100+lvl,'isUserHolding':'1', 'page': 1});
+	xajax_doAttack = function() { return xajax.request( { xjxfun: 'doAttack' }, { parameters: arguments } ); };
+
+xjxargs[]:
+<xjxobj><e><k>generalId</k><v>S27635</v></e><e><k>army</k><v><xjxobj><e><k>P1</k><v>S</v></e><e><k>P2</k><v>S</v></e><e><k>CT</k><v>S</v></e><e><k>S1</k><v>S1001</v></e><e><k>S2</k><v>S</v></e><e><k>C1</k><v>S</v></e><e><k>M1</k><v>S</v></e><e><k>M2</k><v>S</v></e><e><k>C2</k><v>S</v></e><e><k>K2</k><v>S</v></e><e><k>C4</k><v>S</v></e></xjxobj></v></e><e><k>dUserId</k><v>S1007</v></e><e><k>dProvinceId</k><v>S1662</v></e><e><k>attackType</k><v>S1</v></e><e><k>dName</k><v>SNPCHeavyGod_7</v></e><e><k>formation</k><v>S1</v></e><e><k>sendAttack</k><v>S<![CDATA[Oblężenie Twierdzy]]></v></e><e><k>vexok</k><v>Btrue</v></e></xjxobj>
+xjxargs[]:<xjxobj><e><k>generalId</k><v>S</v></e><e><k>army</k><v><xjxobj><e><k>P1</k><v>S</v></e><e><k>P2</k><v>S</v></e><e><k>CT</k><v>S</v></e><e><k>S1</k><v>S1111</v></e><e><k>S2</k><v>S</v></e><e><k>C1</k><v>S</v></e><e><k>M1</k><v>S</v></e><e><k>M2</k><v>S</v></e><e><k>C2</k><v>S</v></e><e><k>K2</k><v>S</v></e><e><k>C4</k><v>S</v></e></xjxobj></v></e><e><k>dUserId</k><v>S1007</v></e><e><k>dProvinceId</k><v>S1662</v></e><e><k>attackType</k><v>S1</v></e><e><k>dName</k><v>SNPCHeavyGod_7</v></e><e><k>formation</k><v>S1</v></e><e><k>sendAttack</k><v>S<![CDATA[Oblężenie Twierdzy]]></v></e><e><k>vexok</k><v>Btrue</v></e></xjxobj>
+xjxargs[]:
+<xjxobj>
+	<e><k>generalId</k><v>S27635</v></e>
+	<e>
+		<k>army</k>
+			<v>
+				<xjxobj>
+					<e>
+						<k>P1</k>
+							<v>S</v>
+						</e><e><k>P2</k><v>S</v></e><e><k>CT</k><v>S</v></e><e><k>S1</k><v>S1111</v></e><e><k>S2</k><v>S</v></e><e><k>C1</k><v>S</v></e><e><k>M1</k><v>S</v></e><e><k>M2</k><v>S</v></e><e><k>C2</k><v>S</v></e><e><k>K2</k><v>S</v></e><e><k>C4</k><v>S</v></e></xjxobj></v></e><e><k>dUserId</k><v>S1007</v></e><e><k>dProvinceId</k><v>S1662</v></e><e><k>attackType</k><v>S1</v></e><e><k>dName</k><v>SNPCHeavyGod_7</v></e><e><k>formation</k><v>S1</v></e><e><k>sendAttack</k><v>S<![CDATA[Oblężenie Twierdzy]]></v></e><e><k>vexok</k><v>Btrue</v></e></xjxobj>
+xjxargs[]:<xjxobj><e><k>generalId</k><v>S27635</v></e><e><k>army</k><v><xjxobj><e><k>P1</k><v>S</v></e><e><k>P2</k><v>S</v></e><e><k>CT</k><v>S</v></e><e><k>S1</k><v>S1111</v></e><e><k>S2</k><v>S</v></e><e><k>C1</k><v>S</v></e><e><k>M1</k><v>S</v></e><e><k>M2</k><v>S</v></e><e><k>C2</k><v>S</v></e><e><k>K2</k><v>S</v></e><e><k>C4</k><v>S</v></e></xjxobj></v></e><e><k>dUserId</k><v>S1007</v></e><e><k>dProvinceId</k><v>S3110</v></e><e><k>attackType</k><v>S1</v></e><e><k>dName</k><v>SNPCHeavyGod_7</v></e><e><k>formation</k><v>S1</v></e><e><k>sendAttack</k><v>S<![CDATA[Oblężenie Twierdzy]]></v></e><e><k>vexok</k><v>Btrue</v></e></xjxobj>
+Name
+
+Name
+
+Name
+
+Name
+	
+
+siegeAttackCheck('$(\'#attackType\').val(\'1\'); xajax_doAttack(\'OperationCenter\', xajax.getFormValues(\'sendAttackForm\'));','xajax_viewConfirmation(container.open({saveName:\'modal\'}),{\'id\':5,\'action\':\'$(\\u0027#attackType\\u0027).val(\\u00271\\u0027); xajax_doAttack(\\u0027OperationCenter\\u0027, xajax.getFormValues(\\u0027sendAttackForm\\u0027));\',\'message\':\'S2F3YWxlcmlhIGkgxYJ1Y3puaWN5IGkgd8OzemtpIHRvd2Fyb3dlIG5pZSB3eXJ6xIVkemFqxIUgc3RyYXR5IGF0YWt1asSFYyBUd2llcmR6eS4gV3lzecWCYWrEhWMga29ubmljeSBpL2x1YiDFgnVjem5pa8OzdyBpL2x1YiB3w7N6a2kgdG93YXJvd2UgdyBvYmzEmcW8ZW5pdSBzdHJhY2lzeiBiaXR3xJkuPGJyLz48YnIvPkN6eSBuYSBwZXdubyBjaGNlc3ogZG9rb25hxIcgYXRhaz8=\',\'allowDeactivation\':true})')
+	}
 xjxargs[]:<xjxobj><e><k>uId</k><v>N107</v></e><e><k>isUserHolding</k><v>S1</v></e><e><k>page</k><v>N1</v></e><e><k>vexok</k><v>Btrue</v></e></xjxobj>
 Name
 
